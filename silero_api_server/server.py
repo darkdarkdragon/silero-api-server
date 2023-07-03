f485852b30f01034e54209a25a1cad5af5070ebd
Ivan Tivonenko <darkdarkdragon@gmail.com>
2023-07-02 21:14:53 -0400

    remove examples serving/generation allow to generate in multiple languages generate files in temporary directory and remove after serving

--- silero_api_server/server.py
+++ silero_api_server/server.py
@@ -1,33 +1,52 @@
-
 import pathlib
 import os
+import tempfile
+import hashlib
 from fastapi import FastAPI, Response, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.background import BackgroundTask
 from pydantic import BaseModel
 import uvicorn
 from silero_api_server.tts import SileroTtsService
 from loguru import logger
-from typing import Optional
+from typing import Optional, Tuple
+from pydub import AudioSegment
 
 module_path = pathlib.Path(__file__).resolve().parent
 os.chdir(module_path)
+print(f"module path: {module_path}")
 SAMPLE_PATH = "samples"
 SESSION_PATH = "sessions"
 
-tts_service = SileroTtsService(f"{module_path}//{SAMPLE_PATH}", SESSION_PATH)
+tts_service = SileroTtsService(
+    f"{module_path}//{SAMPLE_PATH}", SESSION_PATH, languages=["en", "es", "ua"]
+)
+# tts_service = SileroTtsService(f"{module_path}//{SAMPLE_PATH}", SESSION_PATH, languages=['ua'])
 app = FastAPI()
 
 # Make sure the samples directory exists
 if not os.path.exists(SAMPLE_PATH):
     os.mkdir(SAMPLE_PATH)
 
-if len(os.listdir(SAMPLE_PATH)) == 0:
-    logger.info("Samples empty, generating new samples.")
-    tts_service.generate_samples()
+# if len(os.listdir(SAMPLE_PATH)) == 0:
+#     logger.info("Samples empty, generating new samples.")
+#     tts_service.generate_samples()
 
-app.mount(f"/samples",StaticFiles(directory=f"{module_path}//{SAMPLE_PATH}"),name='samples')
+
+# app.mount(f"/samples",StaticFiles(directory=f"{module_path}//{SAMPLE_PATH}"),name='samples')
+def calculate_hashes(file_path: str) -> Tuple[str, str]:
+    md5_hash = hashlib.md5()
+    sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as file:
+        # Read the file in chunks to avoid loading the entire file into memory
+        for chunk in iter(lambda: file.read(4096), b""):
+            md5_hash.update(chunk)
+            sha256_hash.update(chunk)
+    return md5_hash.hexdigest(), sha256_hash.hexdigest()
+
+
 origins = ["*"]
 
 app.add_middleware(
@@ -38,48 +57,91 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Voice(BaseModel):
+    language: str
     speaker: str
     text: str
+    use_ssml: bool = False
     session: Optional[str]
+
 
 class SampleText(BaseModel):
     text: Optional[str]
 
+
 @app.get("/tts/speakers")
 def speakers(request: Request):
-    voices = [
-        {
-            "name":speaker,
-            "voice_id":speaker,
-            "preview_url": f"{str(request.base_url)}{SAMPLE_PATH}/{speaker}.wav"
-        } for speaker in tts_service.get_speakers()
-    ]
+    voices = {
+        lang: [
+            {
+                "name": speaker,
+                "voice_id": speaker,
+                "preview_url": f"{str(request.base_url)}{SAMPLE_PATH}/{speaker}.wav",
+            }
+            for speaker in speaker
+        ]
+        for lang, speaker in tts_service.get_speakers().items()
+    }
     return voices
+
+
+def remove_file(path: str) -> None:
+    os.unlink(path)
+
 
 @app.post("/tts/generate")
 def generate(voice: Voice):
     # Clean elipses
-    voice.text = voice.text.replace("*","")
+    voice.text = voice.text.replace("*", "")
     try:
+        wav_file_name = tempfile.mktemp(suffix=".wav")
         if voice.session:
-            audio = tts_service.generate(voice.speaker, voice.text, voice.session)
+            audio = tts_service.generate(
+                voice.language,
+                voice.speaker,
+                voice.text,
+                voice.session,
+                audio_file_path=wav_file_name,
+                use_ssml=voice.use_ssml,
+            )
         else:
-            audio = tts_service.generate(voice.speaker, voice.text)
-        return FileResponse(audio)
+            audio = tts_service.generate(
+                voice.language,
+                voice.speaker,
+                voice.text,
+                audio_file_path=wav_file_name,
+                use_ssml=voice.use_ssml,
+            )
+        mp3_file_name = tempfile.mktemp(suffix=".mp3")
+        raw = AudioSegment.from_wav(audio)
+        raw.export(mp3_file_name, format="mp3")
+        os.unlink(audio)
+        task = BackgroundTask(remove_file, mp3_file_name)
+        md5, sha256 = calculate_hashes(mp3_file_name)
+        duration_in_milliseconds = len(raw)
+        headers = {
+            "duration": str(duration_in_milliseconds),
+            "md5": md5,
+            "sha256": sha256,
+        }
+        return FileResponse(
+            mp3_file_name, media_type="audio/mpeg", headers=headers, background=task
+        )
     except Exception as e:
         logger.error(e)
-        return HTTPException(500,voice.speaker)
-    
-@app.get("/tts/sample")
-def play_sample(speaker: str):
-    return FileResponse(f"{SAMPLE_PATH}/{speaker}.wav")
+        return HTTPException(500, voice.speaker)
 
-@app.post("/tts/generate-samples")
-def generate_samples(sample_text: Optional[str] = ""):
-    tts_service.update_sample_text(sample_text)
-    tts_service.generate_samples()
-    return Response("Generated samples",status_code=200)
+
+# @app.get("/tts/sample")
+# def play_sample(speaker: str):
+#     return FileResponse(f"{SAMPLE_PATH}/{speaker}.wav")
+
+# @app.post("/tts/generate-samples")
+# def generate_samples(sample_text: Optional[str] = ""):
+#     tts_service.update_sample_text(sample_text)
+#     tts_service.generate_samples()
+#     return Response("Generated samples",status_code=200)
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
--- silero_api_server/tts.py
+++ silero_api_server/tts.py
@@ -1,57 +1,86 @@
 # V3
 import os, time
+import inspect
 import torch
+import tempfile
 import torch.package
 import torchaudio
 from hashlib import md5
 from loguru import logger
 from pydub import AudioSegment
 
+available_models = {
+    'en': 'https://models.silero.ai/models/tts/en/v3_en.pt',
+    'es': 'https://models.silero.ai/models/tts/es/v3_es.pt',
+    'ua': 'https://models.silero.ai/models/tts/ua/v3_ua.pt',
+}
+
 class SileroTtsService:
     """
     Generate TTS wav files using Silero
     """
-    def __init__(self, sample_path, sessions_path="sessions") -> None:
+    def __init__(self, sample_path, sessions_path="sessions", languages = []) -> None:
         self.sample_text = "The fallowed fallen swindle auspacious goats in portable power stations."
         self.sample_path = sample_path
         self.sessions_path = sessions_path
+        self.sample_rate = 48000
+        if len(languages) == 0:
+            raise Exception("Specify languages")
         # Silero works fine on CPU
         self.device = torch.device('cpu')
         torch.set_num_threads(4)
         torchaudio.set_audio_backend("soundfile")
+        self.models = {}
+        for lang in languages:
+            url = available_models[lang]
+            file_name = os.path.basename(url)
+            if not os.path.isfile(file_name):
+                logger.warning(f"First run, downloading Silero model for {lang} language. This could take some time...")
+                torch.hub.download_url_to_file(url, file_name)
+                logger.info(f"Model for {lang} language download completed.")
+            else:
+                self.models[lang] = torch.package.PackageImporter(file_name).load_pickle("tts_models", "model")
+                self.models[lang].to(self.device)
+                # print(f'speakers for {lang}: {self.models[lang].speakers}')
+        # print(f'speakers: {self.get_speakers()}')
+        # audio = self.generate('ua', 'mykyta', "весела блискавка 12 була дуже приємна <prosody pitch=\"x-high\">сто двадцять пʼять разів</prosody>", use_ssml=True)
+        # print(f'got audio={audio}')
+        # audio = self.generate('es', 'es_1', "habia 19")
+        # print(f'got audio={audio}')
 
         # Make sure we  have the model
-        self.local_file = 'model.pt'
-        if not os.path.isfile(self.local_file):
-            logger.warning(f"First run, downloading Silero model. This could take some time...") 
-            torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt',
-                                        self.local_file)  
-            logger.info(f"Model download completed.") 
+        # self.local_file = 'model.pt'
+        # if not os.path.isfile(self.local_file):
+        #     logger.warning(f"First run, downloading Silero model. This could take some time...")
+        #     torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt',
+        #                                 self.local_file)
+        #     logger.info(f"Model download completed.")
 
 
         # Make sure we have the path
-        if not os.path.exists('samples'):
-            os.mkdir('samples')        
-        
-        if not os.path.exists(sessions_path):
-            os.mkdir(sessions_path)
+        # if not os.path.exists('samples'):
+        #     os.mkdir('samples')
 
-        self.model = torch.package.PackageImporter(self.local_file).load_pickle("tts_models", "model")
-        self.model.to(self.device)
+        # if not os.path.exists(sessions_path):
+        #     os.mkdir(sessions_path)
 
-        self.sample_rate = 48000 
-        logger.info(f"TTS Service loaded successfully") 
+        # self.model = torch.package.PackageImporter(self.local_file).load_pickle("tts_models", "model")
+        # self.model.to(self.device)
 
-    def generate(self, speaker, text: str, session=""):
+        logger.info(f"TTS Service loaded successfully")
+
+    def generate(self, lang: str, speaker: str, text: str, session="", audio_file_path = "test.wav", use_ssml = False):
         """
         Generate a TTS wav file and return it. Optional session is provided to save related samples into a folder.
         """
-        logger.info(f"Generating text {text} using speaker {speaker}") 
+        logger.info(f"Generating text {text} using speaker {speaker} in {lang}")
 
         # Character limit seems to be 1000. Split by sentences, clauses, or words, depending if any are longer than limit.
         char_limit = 1000
+        # char_limit = 40
         if len(text) > char_limit:
             logger.warning("Text too long. Splitting by sentences.")
+            wav_file_name = tempfile.mktemp(suffix=".wav")
             str_to_wav = ""
             combined_wav = AudioSegment.empty()
             for sentence in text.split('.'):
@@ -65,7 +94,7 @@ class SileroTtsService:
                     for clause in sentence.split(","):
                         if len(str_to_wav) + len(clause) < char_limit:
                             str_to_wav = ",".join([str_to_wav, clause])
-                        
+
                         # Try to split by word? Seriously? How do you have a clause that's that long? Fuck.
                         elif len(clause) > char_limit:
                             logger.warning("Clause too long. Splitting by words.")
@@ -77,26 +106,43 @@ class SileroTtsService:
                                     raise Exception("No. I'm not going to generate that. Piss off. You know why.")
                                 else:
                                     logger.debug(f"Rendering audio for text with length {len(str_to_wav)}")
-                                    audio = self.model.save_wav(text=str_to_wav,speaker=speaker,sample_rate=self.sample_rate)
+                                    audio = self.models[lang].save_wav(text=str_to_wav,speaker=speaker,sample_rate=self.sample_rate, audio_path=wav_file_name)
                                     combined_wav += AudioSegment.silent(500) # Insert 500ms pause
                                     combined_wav += AudioSegment.from_file(audio)
                                     str_to_wav = word
+                                    os.unlink(wav_file_name)
                         else:
                             logger.debug(f"Rendering audio for text with length {len(str_to_wav)}")
-                            audio = self.model.save_wav(text=str_to_wav,speaker=speaker,sample_rate=self.sample_rate)
+                            audio = self.models[lang].save_wav(text=str_to_wav,speaker=speaker,sample_rate=self.sample_rate, audio_path=wav_file_name)
                             combined_wav += AudioSegment.silent(500) # Insert 500ms pause
                             combined_wav += AudioSegment.from_file(audio)
                             str_to_wav = clause
+                            os.unlink(wav_file_name)
                 else:
                     logger.debug(f"Rendering audio for text with length {len(str_to_wav)}")
-                    audio = self.model.save_wav(text=str_to_wav,speaker=speaker,sample_rate=self.sample_rate)
+                    audio = self.models[lang].save_wav(text=str_to_wav,speaker=speaker,sample_rate=self.sample_rate, audio_path=wav_file_name)
                     combined_wav += AudioSegment.silent(500) # Insert 500ms pause
                     combined_wav += AudioSegment.from_file(audio)
                     str_to_wav = sentence
-            combined_wav.export("test.wav", format="wav")
-            audio = "test.wav"
-        else: 
-            audio = self.model.save_wav(text=text,speaker=speaker,sample_rate=self.sample_rate)
+            combined_wav.export(audio_file_path, format="wav")
+            audio = audio_file_path
+        else:
+            # mo = self.models[lang]
+            # Get method arguments
+            # args = inspect.signature(mo.save_wav).parameters
+            # for arg_name, arg_obj in args.items():
+            #     print("Argument name:", arg_name)
+            #     print("Default value:", arg_obj.default)
+            #     print("Is keyword argument:", arg_obj.kind == inspect.Parameter.KEYWORD_ONLY)
+            #     print()
+            # au = self.models[lang].apply_tts(text=text,speaker=speaker,sample_rate=self.sample_rate)
+            # print(au)
+            if use_ssml:
+                if not text.startswith('<speak>'):
+                    text = '<speak>' + text + '</speak>'
+                audio = self.models[lang].save_wav(ssml_text=text,speaker=speaker,sample_rate=self.sample_rate, audio_path = audio_file_path)
+            else:
+                audio = self.models[lang].save_wav(text=text,speaker=speaker,sample_rate=self.sample_rate, audio_path = audio_file_path)
 
         # Retain wav files grouped by a session
         if session:
@@ -110,26 +156,28 @@ class SileroTtsService:
 
     def get_speakers(self):
         "List different speakers in model"
-        return self.model.speakers
+        return {lang: model.speakers for lang, model in self.models.items()}
 
-    def generate_samples(self):
-        "Remove current samples and generate new ones for all speakers."
-        logger.warning("Removing current samples")
-        for file in os.listdir(self.sample_path):
-            os.remove(f"{self.sample_path}/{file}")
+    # def generate_samples(self):
+    #     "Remove current samples and generate new ones for all speakers."
+    #     logger.warning("Removing current samples")
+    #     for file in os.listdir(self.sample_path):
+    #         os.remove(f"{self.sample_path}/{file}")
 
-        logger.info("Creating new samples. This should take a minute...")
-        for speaker in self.model.speakers: 
-            name = f"{speaker}.wav"
-            if os.path.exists(name):
-                continue
-            audio = self.model.save_wav(text=self.sample_text,speaker=speaker,sample_rate=self.sample_rate)
-            os.rename(audio, f"{self.sample_path}/{name}")
-        logger.info("New samples created")  
+    #     logger.info("Creating new samples. This should take a minute...")
+    #     for speaker in self.model.speakers:
+    #         name = f"{speaker}.wav"
+    #         if os.path.exists(name):
+    #             continue
+    #         audio = self.model.save_wav(text=self.sample_text,speaker=speaker,sample_rate=self.sample_rate)
+    #         os.rename(audio, f"{self.sample_path}/{name}")
+    #     logger.info("New samples created")
 
-    def update_sample_text(self,text: str):
-        "Update the text used to generate samples"
-        if not text: return
-        self.sample_text = text
-        logger.info(f"Sample text updated to {self.sample_text}")  
+    # def update_sample_text(self,text: str):
+    #     "Update the text used to generate samples"
+    #     if not text: return
+    #     self.sample_text = text
+    #     logger.info(f"Sample text updated to {self.sample_text}")
 
+if __name__ == "__main__":
+    tts_service = SileroTtsService(f".", "sessions", languages=['en', 'es', 'ua'])
