9f98befc2d59b21d0b53598883cc8ef0ac0a4e3d
Ivan Tivonenko <darkdarkdragon@gmail.com>
2023-07-02 21:12:41 -0400

    by default run at localhost

--- silero_api_server/__main__.py
+++ silero_api_server/__main__.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(
                     prog='silero_api_server',
                     description='Run Silero within a FastAPI application')
-parser.add_argument('-o','--host', action='store', dest='host', default='0.0.0.0')
+parser.add_argument('-o','--host', action='store', dest='host', default='localhost')
 parser.add_argument('-p','--port', action='store', dest='port', type=int, default=8001)
 parser.add_argument('-s','--session_path', action='store', dest='session_path', type=str, default="sessions")
 
